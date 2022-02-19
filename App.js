46be3e07f7126171f189eb8344b211a5fba58e4a
hoainambeco <namxg1@gmail.com>
2022-02-19 21:21:58 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import {RNCamera, FaceDetector} from 'react-native-camera';
+import {useCamera} from 'react-native-camera-hooks';
+import RNFS from 'react-native-fs';
+export default function Camera() {
+  const [{cameraRef}, {takePicture}] = useCamera(null);
+
+  const captureHandle = async () => {
+    try {
+      const data = await takePicture();
+      console.log('aaaaaaaaa' + data.uri);
+      const filePath = data.uri;
+      const newFilePath = RNFS.ExternalDirectoryPath + '/MyTest.jpg';
+      RNFS.moveFile(filePath, newFilePath)
+        .then(() => {
+          console.log('IMAGE MOVED', filePath, '-- to --', newFilePath);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.body}>
+      <RNCamera
+        ref={cameraRef}
+        type={RNCamera.Constants.Type.back}
+        style={styles.preview}>
+        <Button
+          title="Capture"
+          color="#1eb900"
+          onPress={() => captureHandle()}
+        />
+      </RNCamera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+});
